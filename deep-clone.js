067b5011bbe1f4e325ecd503cd1afa7e8bfee75e
WHYJM <wanghongyuan10@gmail.com>
2024-03-18 22:23:59 +0800

    chore: deep clone

--- deep-clone.js
+++ deep-clone.js
@@ -1,1 +1,23 @@
+function deepClone(obj) {
+  // obj == null
+  // 相当于
+  // if (obj === null  || obj === undefined) {}
+  if (typeof obj !== 'object' || obj == null ) {
+    return obj
+  }
 
+  let result 
+  if (obj instanceof Array) {
+    result = []
+  } else {
+    result = {}
+  }
+
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      return deepClone(obj[key])
+    }
+  }
+
+  return result
+}
